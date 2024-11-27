76acdb78fbece653ef78e36470fca1a37b138183
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-27 16:50:43 +0530

    merge_dict

---
+++ coding-assignment/dict_q1.py
@@ -0,0 +1,6 @@
+#Problem: Write a function that merges two dictionaries. If there are overlapping keys, sum their values.
+
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+merge_dicts(dict1, dict2)  # Returns: {'a': 1, 'b': 5, 'c': 4}
